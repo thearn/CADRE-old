0e0c127f0c61f11159f5cfb85b11c820a745aa3c
Tristan Hearn <tristan.a.hearn@nasa.gov>
2013-10-31 13:52:51 -0400

    fixed case recorder in example

--- example.py
+++ example.py
@@ -5,6 +5,6 @@ top = CADRE_Optimization(n=80, m=40)
 top.driver.recorders = [CSVCaseRecorder(filename='CADRE.csv')]
 printvars = []
 for var in ['Data', 'ConCh', 'ConDs', 'ConS0', 'ConS1', 'SOC']:
-    printvars += ["pt" + str(i) + ".Data" for i in xrange(0)]
+    printvars += ["pt" + str(i) + ".Data" for i in xrange(6)]
 top.driver.printvars = printvars
 top.run()
