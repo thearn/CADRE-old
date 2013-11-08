34ba21908747eb2442ec13fb05bac4f2903a9b30
Tristan Hearn <tristan.a.hearn@nasa.gov>
2013-11-08 14:30:14 -0500

    added print statements to example

--- example.py
+++ example.py
@@ -1,10 +1,12 @@
 from openmdao.lib.casehandlers.api import CSVCaseRecorder
 from CADRE import CADRE_Optimization
 
+print "setting up"
 top = CADRE_Optimization(n=1500, m=300)
 top.driver.recorders = [CSVCaseRecorder(filename='CADRE.csv')]
 printvars = []
 for var in ['Data', 'ConCh', 'ConDs', 'ConS0', 'ConS1', 'SOC']:
     printvars += ["pt" + str(i) + ".Data" for i in xrange(6)]
 top.driver.printvars = printvars
+print "running"
 top.run()
