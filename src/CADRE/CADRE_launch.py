119a7c2fec33df1b5c8b7db9710908cba51a0c79
Tristan Hearn <tristanhearn@gmail.com>
2013-11-09 16:55:54 -0500

    added optimization comparison

--- src/CADRE/CADRE_launch.py
+++ src/CADRE/CADRE_launch.py
@@ -20,13 +20,14 @@ class Uniformity(Component):
         based on excess kurtosis (4th order sample moment).
     """
 
-    def __init__(self):
+    def __init__(self, n):
         super(Uniformity, self).__init__()
+        self.n = n
         self.add('sample', Array(iotype='in'))
-        self.add('k', Float(100, iotype='out'))
+        self.add('k', Float(0., iotype='out'))
 
     def execute(self):
-        self.k = np.abs(kurtosis(self.sample.flatten()) + 6. / 5)
+        self.k = (kurtosis(self.sample.flatten()) + 6. / 5) ** 2
         sys.stdout.flush()
 
 
@@ -49,11 +50,11 @@ class GroundLOC(Component):
 
     def execute(self):
         for i in xrange(self.n):
-            pos = self.r_e2b_I[:3, i]
-            pos = pos / np.linalg.norm(pos) * self.Re
-            g_pos = np.dot(self.O_IE[:, :, i].T, pos)
-            self.lats[i] = np.arcsin(g_pos[2] / self.Re) * self.r2d
-            self.lons[i] = np.arctan2(g_pos[1], g_pos[0]) * self.r2d
+            self.pos = self.r_e2b_I[:3, i]
+            self.npos = self.pos / np.linalg.norm(self.pos) * self.Re
+            self.g_pos = np.dot(self.O_IE[:, :, i].T, self.npos)
+            self.lats[i] = np.arcsin(self.g_pos[2] / self.Re) * self.r2d
+            self.lons[i] = np.arctan2(self.g_pos[1], self.g_pos[0]) * self.r2d
 
 
 class CADRE_Launch(Assembly):
@@ -81,7 +82,7 @@ class CADRE_Launch(Assembly):
 
         #self.add('driver', SLSQPdriver())
 
-        self.add('driver', CONMINdriver())
+        #self.add('driver', CONMINdriver())
 
         # Orbit components
         self.add("Orbit_Initial", Orbit_Initial())
@@ -99,10 +100,10 @@ class CADRE_Launch(Assembly):
         self.add("GroundLOC", GroundLOC(n))
         self.driver.workflow.add("GroundLOC")
 
-        self.add("Lon_uniform", Uniformity())
+        self.add("Lon_uniform", Uniformity(n))
         self.driver.workflow.add("Lon_uniform")
 
-        self.add("Lat_uniform", Uniformity())
+        self.add("Lat_uniform", Uniformity(n))
         self.driver.workflow.add("Lat_uniform")
 
         self.connect("t", "Comm_EarthsSpin.t")
@@ -116,27 +117,71 @@ class CADRE_Launch(Assembly):
         self.connect("GroundLOC.lats", "Lat_uniform.sample")
         self.connect("GroundLOC.lons", "Lon_uniform.sample")
 
-        self.driver.add_objective("Lat_uniform.k + Lon_uniform.k")
-        self.driver.add_parameter(
-            "Orbit_Initial.altPerigee", low=500, high=1000)
-        self.driver.add_parameter(
-            "Orbit_Initial.altApogee", low=500, high=1000)
-        self.driver.add_parameter(
-            "Orbit_Initial.RAAN", low=-180, high=180)
-        self.driver.add_parameter(
-            "Orbit_Initial.Inc", low=0, high=90)
-        self.driver.add_parameter(
-            "Orbit_Initial.argPerigee", low=0, high=90)
-        self.driver.iprint = 0
+        #self.driver.add_objective("Lat_uniform.k + Lon_uniform.k")
+        # self.driver.add_parameter(
+        #     "Orbit_Initial.altPerigee", low=500, high=1000)
+        # self.driver.add_parameter(
+        #     "Orbit_Initial.altApogee", low=500, high=1000)
+        # self.driver.add_parameter(
+        #     "Orbit_Initial.RAAN", low=-180, high=180)
+        # self.driver.add_parameter(
+        #     "Orbit_Initial.Inc", low=0, high=90)
+        # self.driver.add_parameter(
+        #     "Orbit_Initial.argPerigee", low=0, high=90)
 
 if __name__ == "__main__":
     import time
+    from scipy.optimize import fmin, fmin_slsqp
+    a = CADRE_Launch()
+    a.add('driver', SLSQPdriver())
+    a.driver.add_objective("Lat_uniform.k + Lon_uniform.k")
+    a.driver.add_parameter(
+        "Orbit_Initial.altPerigee", low=500, high=1000)
+    a.driver.add_parameter(
+        "Orbit_Initial.altApogee", low=500, high=1000)
+    a.driver.add_parameter(
+        "Orbit_Initial.RAAN", low=-180, high=180)
+    a.driver.add_parameter(
+        "Orbit_Initial.Inc", low=0, high=90)
+    a.driver.add_parameter(
+        "Orbit_Initial.argPerigee", low=0, high=90)
+    tt = time.time()
+    print 30 * "-"
+    print "with OpenMDAO optimizer:"
+    l1, l2 = a.GroundLOC.lats, a.GroundLOC.lons
+    print "min/max lats:", min(l1), max(l1)
+    print "min/max lons:", min(l2), max(l2)
+    print "\n"
+    print(a.Orbit_Initial.altPerigee,
+          a.Orbit_Initial.altApogee,
+          a.Orbit_Initial.RAAN,
+          a.Orbit_Initial.Inc,
+          a.Orbit_Initial.argPerigee)
+    print "Elapsed time: ", time.time() - tt, "seconds"
+    print 30 * "-"
+    print "without OpenMDAO optimizer:"
     a = CADRE_Launch()
     tt = time.time()
-    a.run()
+    c1 = lambda x: x[0] - 500
+    c2 = lambda x: 1000 - x[0]
+    c3 = lambda x: x[1] - 500
+    c4 = lambda x: 1000 - x[1]
+    c5 = lambda x: x[2] + 180
+    c6 = lambda x: 180 - x[2]
+    c7 = lambda x: x[3]
+    c8 = lambda x: 90 - x[3]
+
+    def f(orbit):
+        a.Orbit_Initial.altPerigee = orbit[0]
+        a.Orbit_Initial.altApogee = orbit[1]
+        a.Orbit_Initial.RAAN = orbit[2]
+        a.Orbit_Initial.Inc = orbit[3]
+        a.run()
+        return a.Lat_uniform.k + a.Lon_uniform.k
+    fmin_slsqp(f, [600, 600, 0, 45], ieqcons=[c1, c2, c3, c4, c5, c6, c7, c8])
     l1, l2 = a.GroundLOC.lats, a.GroundLOC.lons
-    print "lats:", min(l1), max(l1)
-    print "lons:", min(l2), max(l2)
+    print "min/max lats:", min(l1), max(l1)
+    print "min/max lons:", min(l2), max(l2)
     print "\n"
     print(a.Orbit_Initial.altPerigee,
           a.Orbit_Initial.altApogee,
