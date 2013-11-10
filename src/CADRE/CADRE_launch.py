dc48557ef1d78430fdb21e018811cab68e857c8c
Tristan Hearn <tristanhearn@gmail.com>
2013-11-10 13:15:20 -0500

    implemented derivatives for uniformity & groundloc

--- src/CADRE/CADRE_launch.py
+++ src/CADRE/CADRE_launch.py
@@ -10,7 +10,7 @@ from openmdao.main.datatypes.api import Float, Array, Int
 
 from comm import Comm_EarthsSpin, Comm_EarthsSpinMtx
 from orbit import Orbit_Initial, Orbit_Dynamics
-from scipy.stats import kurtosis
+from scipy.stats import kurtosis, moment
 
 
 class Uniformity(Component):
@@ -26,7 +26,35 @@ class Uniformity(Component):
         self.add('k', Float(0., iotype='out'))
 
     def execute(self):
-        self.k = (kurtosis(self.sample.flatten()) + 6. / 5) ** 2
+        self.k_ = kurtosis(self.sample.flatten())
+        self.k = (self.k_ + 6. / 5) ** 2
+
+    def linearize(self):
+        self.J = 2 * (self.k_ + 6. / 5) * self.d_kurtosis(self.sample)
+
+    def apply_deriv(self, arg, result):
+        if "sample" in arg and "k" in result:
+            result['k'] += np.dot(self.J, arg['sample'])
+
+    def apply_derivT(self, arg, result):
+        if "sample" in result and "k" in arg:
+            result['sample'] += arg['k'] * self.J
+
+    def d_kurtosis(self, x):
+        return (moment(x, 2) * self.d_moment(x, 4) - 2 * moment(x, 4)
+                * self.d_moment(x, 2)) / moment(x, 2) ** 3
+
+    def d_moment(self, x, p=2):
+        n = float(len(x))
+        return self.d_norm_p(x - np.mean(x), p) / n
+
+    def d_norm_p(self, x, p=2):
+        d = p * np.linalg.norm(x, p) ** (p - 1)
+        d = d * self.d_norm(x, p)
+        return d
+
+    def d_norm(self, x, p=2):
+        return np.abs(x) ** (p - 2) * (x) / (np.linalg.norm(x, p) ** (p - 1))
 
 
 class GroundLOC(Component):
@@ -46,13 +74,92 @@ class GroundLOC(Component):
         self.add('lats', Array(np.zeros(self.n), iotype='out'))
         self.add('lons', Array(np.zeros(self.n), iotype='out'))
 
+    def linearize(self):
+        self.J = np.zeros((self.n, 2, 3))
+        self.J_O_IE = np.zeros((self.n, 2, 3, 3))
+
+        for i in xrange(self.n):
+
+            O = self.O_IE[:,:, i].T
+            r = self.r_e2b_I[:3, i]
+            d = self.r2d
+
+            self.J[i, 0, 0] = self.d_lat(
+                O[2, 0], O[2, 1], O[2, 2], d, r[0], r[1], r[2])
+            self.J[i, 0, 1] = self.d_lat(
+                O[2, 1], O[2, 0], O[2, 2], d, r[1], r[0], r[2])
+            self.J[i, 0, 2] = self.d_lat(
+                O[2, 2], O[2, 1], O[2, 0], d, r[2], r[1], r[0])
+
+            self.J[i, 1, 0] = self.d_lon(O[1, 0], O[1, 1], O[1, 2], d,
+                                         O[0, 0], O[0, 1], O[0, 2], r[0], r[1], r[2])
+            self.J[i, 1, 1] = self.d_lon(O[1, 1], O[1, 0], O[1, 2], d,
+                                         O[0, 1], O[0, 0], O[0, 2], r[1], r[0], r[2])
+            self.J[i, 1, 2] = self.d_lon(O[1, 2], O[1, 1], O[1, 0], d,
+                                         O[0, 2], O[0, 1], O[0, 0], r[2], r[1], r[0])
+
+            # self.J_O_IE[i, 0, 2, 0] = self.d_lat_O_IE(
+            #     r[0], r[1], r[2], d, O[2, 0], O[2, 1], O[2, 2])
+            # self.J_O_IE[i, 0, 2, 1] = self.d_lat_O_IE(
+            #     r[1], r[0], r[2], d, O[2, 1], O[2, 0], O[2, 2])
+            # self.J_O_IE[i, 0, 2, 2] = self.d_lat_O_IE(
+            #     r[2], r[1], r[0], d, O[2, 2], O[2, 1], O[2, 0])
+
+            # self.J_O_IE[i, 1, 0, 0] = self.d_lon_O_IE(r[0], r[1], r[2], d,
+            #                                           O[1, 0], O[1, 1],
+            #                                           O[1, 2], O[0, 0],
+            #                                           O[0, 1], O[0, 2])
+
+            # self.J_O_IE[i, 1, 0, 1] = self.d_lon_O_IE(r[1], r[0], r[2], d,
+            #                                           O[1, 0], O[1, 1],
+            #                                           O[1, 2], O[0, 1],
+            #                                           O[1, 0], O[0, 2])
+
+    def apply_deriv(self, arg, result):
+        if 'r_e2b_I' in arg:
+            result['lats'] += np.dot(self.J[:, 0,:], arg['r_e2b_I'][:3])
+            result['lons'] += np.dot(self.J[:, 1,:], arg['r_e2b_I'][:3])
+
+    def apply_derivT(self, arg, result):
+
+        if 'lats' in arg:
+            result['r_e2b_I'][0,:] += arg['lats'] * self.J[:, 0, 0]
+            result['r_e2b_I'][1,:] += arg['lats'] * self.J[:, 0, 1]
+            result['r_e2b_I'][2,:] += arg['lats'] * self.J[:, 0, 2]
+        if 'lons' in arg:
+            result['r_e2b_I'][0,:] += arg['lons'] * self.J[:, 1, 0]
+            result['r_e2b_I'][1,:] += arg['lons'] * self.J[:, 1, 1]
+            result['r_e2b_I'][2,:] += arg['lons'] * self.J[:, 1, 2]
+
+    def d_lat(self, a, b, c, d, x, y, z):
+        return -(d * (-a * (y ** 2 + z ** 2) + b * x * y + c * x * z)) / (np.sqrt((x ** 2 + y ** 2 + z ** 2) ** 3) *
+                                                                          np.sqrt(-((a ** 2 - 1) * x ** 2 + 2 * a * x * (b * y + c * z) + (b ** 2 - 1) * y ** 2 + 2 * b * c * y * z + (c ** 2 - 1) * z ** 2) / (x ** 2 + y ** 2 + z ** 2)))
+
+    def d_lon(self, a, b, c, d, e, f, g, x, y, z):
+        num = -(d * (-a * (f * y + g * z) + b * e * y + c * e * z))
+        denom = (a ** 2 * x ** 2 + 2 * a * x * (b * y + c * z) + b ** 2 * y ** 2 + 2 * b * c * y * z + c ** 2 *
+                 z ** 2 + e ** 2 * x ** 2 + 2 * e * x * (f * y + g * z) + f ** 2 * y ** 2 + 2 * f * g * y * z + g ** 2 * z ** 2)
+        if not denom:
+            return 0.
+        else:
+            return num / denom
+
+    def d_lat_O_IE(self, a, b, c, d, x, y, z):
+        return d * a / (np.sqrt(a ** 2 + b ** 2 + c ** 2) * np.sqrt(1 - (a * x + b * y + c * z) ** 2 / (a ** 2 + b ** 2 + c ** 2)))
+
+    def d_lon_O_IE(self, a, b, c, d, w, v, u, x, y, z,):
+        return -d * (a * (a * w + b * v + c * u)) / (a ** 2 * (w ** 2 + x ** 2) + 2 * c * (a * u * w + a * x * z + b * u * v + b * y * z) + 2 * a * b * (v * w + x * y) + b ** 2 * (v ** 2 + y ** 2) + c ** 2 * (u ** 2 + z ** 2))
+
     def execute(self):
+        self.npos = np.zeros((self.n, 3))
+        self.g_pos = np.zeros((self.n, 3))
         for i in xrange(self.n):
             self.pos = self.r_e2b_I[:3, i]
-            self.npos = self.pos / np.linalg.norm(self.pos) * self.Re
-            self.g_pos = np.dot(self.O_IE[:, :, i].T, self.npos)
-            self.lats[i] = np.arcsin(self.g_pos[2] / self.Re) * self.r2d
-            self.lons[i] = np.arctan2(self.g_pos[1], self.g_pos[0]) * self.r2d
+            self.npos[i,:] = self.pos / np.linalg.norm(self.pos, 2) * self.Re
+            self.g_pos[i,:] = np.dot(self.O_IE[:,:, i].T, self.npos[i,:])
+            self.lats[i] = np.arcsin(self.g_pos[i, 2] / self.Re) * self.r2d
+            self.lons[i] = np.arctan2(
+                self.g_pos[i, 1], self.g_pos[i, 0]) * self.r2d
 
 
 class CADRE_Launch(Assembly):
