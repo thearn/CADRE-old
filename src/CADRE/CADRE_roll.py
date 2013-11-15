9a052e834ad340681a5f47580dcd58df72433748
Tristan Hearn <tristan.a.hearn@nasa.gov>
2013-11-15 16:28:34 -0500

    updated roll example

--- src/CADRE/CADRE_roll.py
+++ src/CADRE/CADRE_roll.py
@@ -4,6 +4,7 @@ import numpy as np
 from CADRE import CADRE
 from pyopt_driver import pyopt_driver
 from openmdao.lib.drivers.api import CONMINdriver
+from openmdao.lib.drivers.api import SLSQPdriver
 import os
 
 
@@ -13,14 +14,17 @@ class Deflection(Component):
         super(Deflection, self).__init__()
         self.add('r_b2g_A', Array(np.zeros((3, n)), iotype='in',
                                   shape=(3, n)))
-        self.add("d", Array(np.zeros(n), iotype='in'))
+        #self.add("d", Array(np.zeros(n), iotype='out'))
+        self.add("d", Float(1., iotype='out'))
+        self.n = n
+        self.nd = np.zeros(n)
 
     def linearize(self):
-        self.J = np.zeros((3, n))
+        self.J = np.zeros((3, self.n))
 
-        self.J[0] = self.r_b2g_A[0] / d
-        self.J[1] = self.r_b2g_A[1] / d
-        self.J[2] = self.r_b2g_A[2] / d
+        self.J[0] = self.r_b2g_A[0] / self.nd
+        self.J[1] = self.r_b2g_A[1] / self.nd
+        self.J[2] = self.r_b2g_A[2] / self.nd
 
     def apply_deriv(self, arg, result):
         if 'r_b2g_A' in arg:
@@ -33,7 +37,8 @@ class Deflection(Component):
             result['r_b2g_A'][2, :] += arg['d'] * self.J[2]
 
     def execute(self):
-        self.d = np.sqrt(np.sum(self.r_b2g_A ** 2, axis=0))
+        self.nd = np.sqrt(np.sum(self.r_b2g_A ** 2, axis=0))
+        self.d = sum(self.nd)
 
 
 class CADRE_Roll(Assembly):
@@ -42,12 +47,12 @@ class CADRE_Roll(Assembly):
         super(CADRE_Roll, self).__init__()
 
         # add SNOPT driver
-        self.add("driver", pyopt_driver.pyOptDriver())
-        self.driver.optimizer = "SNOPT"
-        self.driver.options = {'Major optimality tolerance': 1e-8,
-                               'Iterations limit': 500000000}
+        #self.add("driver", pyopt_driver.pyOptDriver())
+        #self.driver.optimizer = "SNOPT"
+        # self.driver.options = {'Major optimality tolerance': 1e-8,
+        #                       'Iterations limit': 500000000}
 
-        #self.add("driver", CONMINdriver())
+        #self.add("driver", SLSQPdriver())
 
         # orbit position and velocity data for each design point
         r_e2b_I0 = [-4969.91222,  4624.84149,
@@ -57,23 +62,40 @@ class CADRE_Roll(Assembly):
         LD = 5417.5
 
         self.add("CADRE", CADRE(n, m))
+        self.driver.workflow.add("CADRE")
         self.CADRE.set("LD", LD)
         self.CADRE.set("r_e2b_I0", r_e2b_I0)
 
         self.add("Deflection", Deflection(n))
         self.driver.workflow.add("Deflection")
 
-        self.CADRE.create_passthrough("Comm_VectorAnt.r_b2g_A")
-        self.connect("CADRE.r_b2g_A", "Deflection.r_b2g_A")
+        self.CADRE.create_passthrough("Comm_VectorBody.r_b2g_B")
+        self.connect("CADRE.r_b2g_B", "Deflection.r_b2g_A")
 
         # add parameters to driver
 
-        self.driver.add_parameter("CADRE.CP_gamma", low=0,
-                                  high=np.pi / 2.)
+        # self.driver.add_parameter("CADRE.CP_gamma", low=0,
+        #                          high=np.pi / 2.)
 
         # add objective
-        self.driver.add_objective("sum(Deflection.d)")
+        # self.driver.add_objective("Deflection.d")
 
 if __name__ == "__main__":
+    import pylab
+    import pickle
     a = CADRE_Roll()
     a.run()
+    pylab.figure()
+    pylab.subplot(211)
+    pylab.plot(a.Deflection.nd)
+    # pylab.plot(a.CADRE.BsplineParameters.CP_gamma)
+    print a.Deflection.d
+
+    a.CADRE.CP_gamma = pickle.load(
+        open("src/CADRE/test/data1346.pkl"))["3:CP_gamma"]
+    a.run()
+    print a.Deflection.d
+    pylab.subplot(212)
+    pylab.plot(a.Deflection.nd)
+    # pylab.plot(a.CADRE.BsplineParameters.CP_gamma)
+    pylab.show()
