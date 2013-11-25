Updated units and desc. Fixed bugs

src/CADRE/parameters.py
@@ -13,8 +13,8 @@ class BsplineParameters(Component):
 
         self.n = n
         self.m = m
-        self.add('t1', units="s", desc="Start time", Float(0., iotype='in'))
-        self.add('t2', units="s", desc="End time", Float(43200., iotype='in'))
+        self.add('t1', Float(0., units='s', desc='Start time', iotype='in'))
+        self.add('t2',  Float(43200., units='s', desc='End time', iotype='in'))
 
         self.B = MBI(np.zeros(n),
                      [np.linspace(self.t1,self.t2,n)], [self.m], [4]).getJacobian(0,0)

src/CADRE/power.py
@@ -200,7 +200,7 @@ class Power_Total(Component):
         self.add(
             'P_sol', Array(np.zeros((n, ), order='F'), size=(n,), dtype=np.float,
                            units='W',
-                           desc='Power from solar panels'
+                           desc='Power from solar panels',
                            iotype="in"))
         self.add(
             'P_comm', Array(np.zeros((n, ), order='F'), size=(n,), dtype=np.float,

src/CADRE/reactionwheel.py
@@ -7,18 +7,31 @@ import rk4
 
 
 class ReactionWheel_Motor(Component):
-
+    '''Compute motor torque'''
+    
     def __init__(self, n):
         super(ReactionWheel_Motor, self).__init__()
         self.n = n
 
         self.add('J_RW', 2.8e-5)
 
-        self.add('T_RW', Array(np.zeros((3,n)), size=(3,n), dtype=np.float, iotype='in'))
-        self.add('w_B', Array(np.zeros((3,n)), size=(3,n), dtype=np.float, iotype='in'))
-        self.add('w_RW', Array(np.zeros((3,n)), size=(3,n), dtype=np.float, iotype='in'))
+        self.add('T_RW', Array(np.zeros((3,n)), size=(3,n),
+                               units="kg*m**2",
+                               desc="Torque vector of reaction wheel",
+                               dtype=np.float, iotype='in'))
+        self.add('w_B', Array(np.zeros((3,n)), size=(3,n),
+                              units="1/s",
+                              desc="Angular velocity vector of satellite",
+                              dtype=np.float, iotype='in'))
+        self.add('w_RW', Array(np.zeros((3,n)), size=(3,n),
+                               units="1/s",
+                               desc="Angular velocity vector of reaction wheel",
+                               dtype=np.float, iotype='in'))
 
-        self.add('T_m', Array(np.ones((3,n)), size=(3,n), dtype=np.float, iotype='out'))
+        self.add('T_m', Array(np.ones((3,n)), size=(3,n),
+                              units="kg*m**2",
+                              desc="Torque vector of motor",
+                              dtype=np.float, iotype='out'))
 
     def linearize(self):
         w_Bx = np.zeros((3,3))
@@ -85,7 +98,8 @@ class ReactionWheel_Motor(Component):
 
 
 class ReactionWheel_Power(Component):
-
+    '''Compute reaction wheel power'''
+    
     #constants
     V = 4.0
     a = 4.9e-4
@@ -96,10 +110,19 @@ class ReactionWheel_Power(Component):
         super(ReactionWheel_Power, self).__init__()
         self.n = n
 
-        self.add('w_RW', Array(np.zeros((3,n)), size=(3,n), dtype=np.float, iotype='in'))
-        self.add('T_RW', Array(np.zeros((3,n)), size=(3,n), dtype=np.float, iotype='in'))
+        self.add('w_RW', Array(np.zeros((3,n)), size=(3,n),
+                               units="1/s",
+                               desc="Angular velocity vector of reaction wheel",
+                               dtype=np.float, iotype='in'))
+        self.add('T_RW', Array(np.zeros((3,n)), size=(3,n),
+                               units="kg*m**2",
+                               desc="Torque vector of reaction wheel",
+                               dtype=np.float, iotype='in'))
 
-        self.add('P_RW', Array(np.ones((3,n)), size=(3,n), dtype=np.float, iotype='out'))
+        self.add('P_RW', Array(np.ones((3,n)), size=(3,n),
+                               units='W',
+                               desc='Reaction wheel power',
+                               dtype=np.float, iotype='out'))
 
     def linearize(self):
         self.dP_dw = np.zeros((self.n,3))
@@ -135,14 +158,21 @@ class ReactionWheel_Power(Component):
 
 
 class ReactionWheel_Torque(Component):
-
+    '''Compute reaction wheel torque'''
+    
     def __init__(self, n):
         super(ReactionWheel_Torque, self).__init__()
         self.n = n
 
-        self.add('T_tot', Array(np.zeros((3,n)), size=(3,n), dtype=np.float, iotype='in'))
+        self.add('T_tot', Array(np.zeros((3,n)), size=(3,n),
+                                units='kg*m**2',
+                                desc='Total torque',
+                                dtype=np.float, iotype='in'))
 
-        self.add('T_RW', Array(np.zeros((3,n)), size=(3,n), dtype=np.float, iotype='out'))
+        self.add('T_RW', Array(np.zeros((3,n)), size=(3,n),
+                               units="kg*m**2",
+                               desc="Torque vector of reaction wheel",
+                               dtype=np.float, iotype='out'))
 
     def linearize(self):
         """ Calculate and save derivatives. (i.e., Jacobian) """
@@ -169,13 +199,21 @@ class ReactionWheel_Dynamics(rk4.RK4):
         super(ReactionWheel_Dynamics, self).__init__()
 
         self.add('w_B', Array(np.zeros((3, n_times)), size=(3, n_times),
+                              units="1/s",
+                              desc="Angular velocity vector of satellite",
                               dtype=np.float, iotype='in'))
         self.add('T_RW', Array(np.zeros((3, n_times)), size=(3, n_times),
+                               units="kg*m**2",
+                               desc="Torque vector of reaction wheel",
                                dtype=np.float, iotype='in'))
 
         self.add('w_RW', Array(np.zeros((3, n_times)), size=(3, n_times),
+                               units="1/s",
+                               desc="Angular velocity vector of reaction wheel",
                                dtype=np.float, iotype='out'))
         self.add('w_RW0', Array(np.zeros((3,)), size=(3,),
+                                units="1/s",
+                                desc="Initial angular velocity vector of reaction wheel",
                                 dtype=np.float, iotype='in'))
 
         self.state_var = 'w_RW'