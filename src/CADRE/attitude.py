Fixed all units. Tests pass.

src/CADRE/attitude.py
@@ -549,10 +549,9 @@ class Attitude_Sideslip(Component):
         # Outputs
         self.add('v_e2b_B', Array(np.zeros((3, n)),
 		                  iotype='out',
-                                  shape=(3, n)),
+                                  shape=(3, n),
                                   units="m/s",
-                                  desc="Velocity vector from body to earth w.r.t body-fixed frame"
-                                  )
+                                  desc="Velocity vector from body to earth w.r.t body-fixed frame"))
 
     def linearize(self):
         """ Calculate and save derivatives. (i.e., Jacobian) """

src/CADRE/parameters.py
@@ -13,39 +13,62 @@ class BsplineParameters(Component):
 
         self.n = n
         self.m = m
-        self.add('t1', Float(0., units='s', desc='Start time', iotype='in'))
-        self.add('t2',  Float(43200., units='s', desc='End time', iotype='in'))
+        self.add('t1', Float(0.,
+		             units='s',
+			     desc='Start time',
+			     iotype='in'))
+
+        self.add('t2',  Float(43200.,
+		              units='s',
+			      desc='End time',
+			      iotype='in'))
 
         self.B = MBI(np.zeros(n),
                      [np.linspace(self.t1,self.t2,n)], [self.m], [4]).getJacobian(0,0)
+
         self.Bdot = MBI(np.zeros(n),
                         [np.linspace(self.t1,self.t2,n)], [self.m], [4]).getJacobian(1,0)
+
         self.BT = self.B.transpose()
         self.BdotT = self.Bdot.transpose()
 
-        self.add('CP_P_comm', Array(np.zeros((self.m,)), size=(self.m,), dtype=float,
+        self.add('CP_P_comm', Array(np.zeros((self.m,)),
+		                    size=(self.m,),
+				    dtype=float,
                                     units='W',
                                     desc='Communication power at the control points',
                                     iotype='in'))
-        self.add('CP_gamma', Array(np.zeros((self.m,)), size=(self.m,), dtype=float,
+
+        self.add('CP_gamma', Array(np.zeros((self.m,)),
+		                   size=(self.m,),
+				   dtype=float,
                                    units='rad',
                                    desc='satellite roll angle at control points',
                                    iotype='in'))
-        self.add('CP_Isetpt', Array(np.zeros((12,self.m)), size=(12,self.m),
+
+        self.add('CP_Isetpt', Array(np.zeros((12,self.m)),
+		                    size=(12,self.m),
                                     dtype=float,
                                     units='A',
                                     desc='Currents of the solar panels at the control points',
                                     iotype='in'))
 
-        self.add('P_comm', Array(np.ones((n,)), size=(n,), dtype=float,
-                                    units='W',
-                                    desc='Communication power',
+        self.add('P_comm', Array(np.ones((n,)),
+		                 size=(n,), dtype=float,
+                                 units='W',
+                                 desc='Communication power',
                                  iotype='out'))
-        self.add('Gamma', Array(0.1*np.ones((n,)), size=(n,), dtype=float,
+
+        self.add('Gamma', Array(0.1*np.ones((n,)),
+		                size=(n,),
+				dtype=float,
                                 units='rad',
                                 desc='satellite roll angle',
                                 iotype='out'))
-        self.add('Isetpt',Array(0.2*np.ones((12,n)), size=(12,n), dtype=float,
+
+        self.add('Isetpt',Array(0.2*np.ones((12,n)),
+		                size=(12,n),
+				dtype=float,
                                 units="A",
                                 desc="Currents of the solar panels",
                                 iotype='out'))

src/CADRE/thermal_temperature.py
@@ -31,44 +31,57 @@ class ThermalTemperature(RK4):
         super(ThermalTemperature, self).__init__()
 
         # Inputs
-        self.add("T0", Array(273.*np.ones((5,)), shape=(5,), dtype=np.float,
-            units="degK",
-            iotype="in", desc="initial temperatures for the 4 fins and body")
+        self.add("T0", Array(273.*np.ones((5,)),
+		             shape=(5,),
+			     dtype=np.float,
+			     units="degK",
+                             iotype="in",
+			     desc="initial temperatures for the 4 fins and body")
         )
 
         self.add("exposedArea", Array(np.zeros((7, 12, n_times)),
                                       units="m**2",
-                                      size=(7, 12 ,n_times), dtype=np.float,
+                                      size=(7, 12 ,n_times),
+				      dtype=np.float,
                                       iotype="in",
                                       desc="exposed area for each solar cell")
         )
 
-        self.add("cellInstd", Array(np.ones((7, 12)), size=(7, 12),
-                                    dtype=np.float, iotype="in",
+        self.add("cellInstd", Array(np.ones((7, 12)),
+		                    size=(7, 12),
+                                    dtype=np.float,
+				    iotype="in",
                                     units='unitless',
                                     desc="Cell/Radiator indication",
-                                    low=0, high=1)
+                                    low=0,
+				    high=1)
         )
 
-        self.add("LOS", Array(np.zeros((n_times, )), size=(n_times, ),
-                              dtype=np.float, iotype="in",
+        self.add("LOS", Array(np.zeros((n_times, )),
+		              size=(n_times, ),
+                              dtype=np.float,
+			      iotype="in",
                               units='unitless',
-                              desc="Line of sight to the sun", low=0, high=1)
+                              desc="Line of sight to the sun",
+			      low=0,
+			      high=1)
         )
 
-        self.add("P_comm", Array(np.ones((n_times, )), size=(n_times, ),
-                                 dtype=np.float, iotype="in",
-                                 units='unitless',
-                                 desc="Power required by the communication " + \
-                                 "system", low=0, high=1)
+        self.add("P_comm", Array(np.ones((n_times, )),
+		                 size=(n_times, ),
+                                 dtype=np.float,
+				 iotype="in",
+                                 units='W',
+                                 desc="Power required by the communication system",
+				 low=0,
+				 high=1)
         )
 
         # Outputs
         self.add("temperature", Array(np.zeros((5, n_times)),
                                       shape=(5, n_times), dtype=np.float,
                                       units="degK",
-                                      iotype="out", desc="temperature for" + \
-                                      " the 4 fins and body over time",
+                                      iotype="out", desc="temperature for the 4 fins and body over time",
                                       low=50, high=400)
         )
 