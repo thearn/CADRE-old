91e0b72547d6b7195a9eeb3cfd33213f1876d027
Justin Gray <justin.s.gray@gmail.com>
2014-04-30 14:50:44 -0400

    updated to use latest case recorder api

--- example.py
+++ example.py
@@ -3,10 +3,10 @@ from CADRE import CADRE_Optimization
 
 print "setting up"
 top = CADRE_Optimization(n=1500, m=300)
-top.driver.recorders = [CSVCaseRecorder(filename='CADRE.csv')]
+top.recorders = [CSVCaseRecorder(filename='CADRE.csv')]
 printvars = []
 for var in ['Data', 'ConCh', 'ConDs', 'ConS0', 'ConS1', 'SOC']:
     printvars += ["pt" + str(i) + ".Data" for i in xrange(6)]
-top.driver.printvars = printvars
+top.printvars = printvars
 print "running"
 top.run()
